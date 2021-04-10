9c78336acfbd8ec583820346b5d7ba7e8b5f484a
TheSharpOwl <ziadxkabakibi@hotmail.com>
2021-04-10 03:07:16 +0300

    Now it is working and the window is there but not showing on top, can see it in the taskbar.

--- Application.cpp
+++ Application.cpp
@@ -12,16 +12,16 @@ RECT WindowRect;
 
 const wchar_t* windowClassName = L"MizuWindowClass";
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
 		case WM_CLOSE:
-			PostQuitMessage(1);
+			::PostQuitMessage(69);
 			break;
 	}
 
-	return DefWindowProc(hWnd, message, wParam, lParam);
+	return ::DefWindowProc(hWnd, message, wParam, lParam);
 }
 
 void RegisterWindowClass(HINSTANCE hInstance, const wchar_t* windowClassName)
@@ -33,30 +33,37 @@ void RegisterWindowClass(HINSTANCE hInstance, const wchar_t* windowClassName)
 	windowClass.cbClsExtra = 0;
 	windowClass.cbWndExtra = 0;
 	windowClass.hInstance = hInstance;
-	windowClass.hIcon = ::LoadIcon(hInstance, NULL);
-	windowClass.hCursor = ::LoadCursor(NULL, IDC_ARROW);
-	windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	windowClass.hIcon = nullptr; //::LoadIcon(hInstance, NULL);
+	windowClass.hCursor = nullptr;//::LoadCursor(NULL, IDC_ARROW);
+	windowClass.hbrBackground = nullptr;//(HBRUSH)(COLOR_WINDOW + 1);
 	windowClass.lpszMenuName = nullptr;
 	windowClass.lpszClassName = windowClassName;
 	windowClass.hIconSm = nullptr;
 
 	static HRESULT hl = ::RegisterClassExW(&windowClass);
 	assert(SUCCEEDED(hl));
+
 }
 
-
+RECT g_WindowRect;
 
 HWND CreateWindow(const wchar_t* windowClassName, const wchar_t* windowTitle, HINSTANCE hInst, uint32_t windowWidth, uint32_t windowHeight)
 {
 	int screenWidth = ::GetSystemMetrics(SM_CXBORDER);
 	int screenHeight = ::GetSystemMetrics(SM_CYBORDER);
 
+	RECT windowRect = { 0, 0, static_cast<LONG>(windowWidth), static_cast<LONG>(windowHeight) };
+	::AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+	windowWidth = windowRect.right - windowRect.left;
+	windowHeight = windowRect.bottom - windowRect.top;
+
 	// putting the window in the center
-	int windowX = std::clamp<int>((screenWidth - windowWidth) / 2, 0, screenWidth);
-	int windowY = std::clamp<int>((screenHeight - windowHeight) / 2, 0, screenHeight);
-
-	HWND hWnd = ::CreateWindowExW(NULL, windowClassName, L"Mizu", WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, windowX, windowY, windowWidth, windowHeight, NULL, NULL, hInst, nullptr);
-
+	int windowX = std::max<int>(0, (screenWidth - windowWidth) / 2);
+	int windowY = std::max<int>(0, (screenHeight - windowHeight) / 2);
+	DWORD d2 = GetLastError();
+	HWND hWnd = ::CreateWindowExW(NULL, windowClassName, L"Mizu", WS_OVERLAPPEDWINDOW, windowX, windowY, windowWidth, windowHeight, NULL, NULL, hInst, nullptr);
+	DWORD d = GetLastError();
 	assert(hWnd && "Failed To create a window");
 	return hWnd;
 }
@@ -66,7 +73,10 @@ int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdL
 {
 	const wchar_t* windowClassName = L"MizuWindowClass";
 	RegisterWindowClass(hInstance, windowClassName);
-	HWND hWnd = CreateWindow(windowClassName, L"Mizu", hInstance, 1280, 720);
-	//ShowWindow( hWnd,SW_SHOW );
+	HWND hWnd = CreateWindow(windowClassName, L"Mizu", hInstance, 300, 300);
+	::ShowWindow(hWnd, SW_SHOW);
+	while (1)
+	{
+	}
 	return 0;
 }	
