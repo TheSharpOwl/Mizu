f061c8efbc078df2e3579532b127285024dc1e94
TheSharpOwl <ziadxkabakibi@hotmail.com>
2021-05-25 23:16:57 +0300

    Added Adapter Creation (compiles but not sure about correctness until creating a device since it's not called).

--- Application.cpp
+++ Application.cpp
@@ -5,6 +5,18 @@
 #include <algorithm>
 #include "Helpers.h"
 
+// DirectX 12 specific headers.
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+// D3D12 extension library.
+#include "d3dx12.h"
+
+#include "wrl.h"
+
+using namespace Microsoft::WRL;
 uint32_t width = 1280, height= 720;
 HWND hWnd;
 // to toggle fullscreen state
@@ -35,7 +47,7 @@ void RegisterWindowClass(HINSTANCE hInstance, const wchar_t* windowClassName)
 	windowClass.hInstance = hInstance;
 	windowClass.hIcon = nullptr; //::LoadIcon(hInstance, NULL);
 	windowClass.hCursor = nullptr;//::LoadCursor(NULL, IDC_ARROW);
-	windowClass.hbrBackground = nullptr;//(HBRUSH)(COLOR_WINDOW + 1);
+	windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 	windowClass.lpszMenuName = nullptr;
 	windowClass.lpszClassName = windowClassName;
 	windowClass.hIconSm = nullptr;
@@ -68,6 +80,54 @@ HWND CreateWindow(const wchar_t* windowClassName, const wchar_t* windowTitle, HI
 	return hWnd;
 }
 
+void EnableDebugLayer()
+{
+#if defined(_DEBUG)
+	ComPtr<ID3D12Debug> debugInterface;
+	ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugInterface)));
+	debugInterface->EnableDebugLayer();
+#endif
+}
+
+
+ComPtr<IDXGIAdapter4> GetAdapter(bool useWarp)
+{
+	ComPtr<IDXGIFactory4> dFactory;
+	UINT createFactoryFlags = 0;
+#if defined(_DEBUG)
+	createFactoryFlags = DXGI_CREATE_FACTORY_DEBUG;
+#endif
+	// we use create factory 2 function since there's no such one for 4
+	ThrowIfFailed(CreateDXGIFactory2(createFactoryFlags, IID_PPV_ARGS(&dFactory)));
+
+	ComPtr<IDXGIAdapter1>dAdapter1;
+	ComPtr<IDXGIAdapter4>dAdapter4;
+
+	if (useWarp)
+	{
+		ThrowIfFailed(dFactory->EnumWarpAdapter(IID_PPV_ARGS(&dAdapter1)));
+		ThrowIfFailed(dAdapter1.As(&dAdapter4));
+	}
+	else
+	{
+		SIZE_T bestVideoMemory = 0;
+		for (unsigned int i = 0; i < dFactory->EnumAdapters1(i, &dAdapter1) != DXGI_ERROR_NOT_FOUND; i++)
+		{
+			DXGI_ADAPTER_DESC1 dAdapterDesc;
+			dAdapter1->GetDesc1(&dAdapterDesc);
+
+			if ((dAdapterDesc.Flags && DXGI_ADAPTER_FLAG_SOFTWARE) == 0 &&
+				SUCCEEDED(D3D12CreateDevice(dAdapter1.Get(), D3D_FEATURE_LEVEL_11_0, __uuidof(ID3D12Device), nullptr)) &&
+				dAdapterDesc.DedicatedVideoMemory > bestVideoMemory)
+			{
+				bestVideoMemory = dAdapterDesc.DedicatedVideoMemory;
+				ThrowIfFailed(dAdapter1.As(&dAdapter4));
+			}
+		}
+	}
+
+	return dAdapter4;
+}
 
 int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
 {
