2eb8b7e470cbbfc42b030c8b81f8114437f2d8d5
TheSharpOwl <ziadxkabakibi@hotmail.com>
2021-07-24 18:47:57 +0300

    Updated WIndow Class.

--- inc/Window.hpp
+++ inc/Window.hpp
@@ -9,11 +9,17 @@ namespace Mizu
 
 		Window(const wchar_t* windowClassName, const wchar_t* windowTitle, HINSTANCE hInst, uint32_t windowWidth, uint32_t windowHeight);
 
+		const uint32_t numberOfBuffers = 2;
+
+		static bool checkTearingSupport();
+
 	protected:
 
 		friend LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 		void RegisterWindowClass(HINSTANCE hInstance, const wchar_t* windowClassName);
+
+		Microsoft::WRL::ComPtr<IDXGISwapChain4> CreateSwapChain(Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue);
 
 		HWND hWnd;
 
--- src/Window.cpp
+++ src/Window.cpp
@@ -1,6 +1,7 @@
 #include "Window.hpp"
 
 using namespace Mizu;
+using namespace Microsoft::WRL;
 
 Window::Window(const wchar_t* windowClassName, const wchar_t* windowTitle, HINSTANCE hInst, uint32_t windowWidth, uint32_t windowHeight) :
 	screenWidth(::GetSystemMetrics(SM_CXSCREEN)), screenHeight(::GetSystemMetrics(SM_CYSCREEN))
@@ -42,4 +43,60 @@ void Window::RegisterWindowClass(HINSTANCE hInstance, const wchar_t* windowClass
 
 	static HRESULT hl = ::RegisterClassExW(&windowClass);
 	assert(SUCCEEDED(hl));
+}
+
+
+bool Window::checkTearingSupport()
+{
+	// using factory 1.4 then 1.5 to enable graphics debugging tool which are not supported (at least until the tutorial date so I will check it later) TODO
+	BOOL allowTearing = FALSE;
+	ComPtr<IDXGIFactory4> f4;
+	if (SUCCEEDED(CreateDXGIFactory1(IID_PPV_ARGS(&f4))))
+	{
+		ComPtr<IDXGIFactory5> f5;
+		if (SUCCEEDED(f4.As(&f5)))
+		{
+			if (FAILED(f5->CheckFeatureSupport(DXGI_FEATURE_PRESENT_ALLOW_TEARING, &allowTearing, sizeof(allowTearing))))
+			{
+				allowTearing = FALSE;
+			}
+		}
+	}
+
+	return allowTearing == TRUE;
+}
+
+ComPtr<IDXGISwapChain4> Window::CreateSwapChain(Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue)
+{
+	ComPtr<IDXGIFactory4> f4;
+	// well we have to create a swapchain1 and cast it to a swapchain4 because the CreateSwapChainForHwnd func takes swapchain1.......
+	ComPtr<IDXGISwapChain1> swapChain1;
+	ComPtr<IDXGISwapChain4> swapChain4;
+
+	UINT createFactoryFlags = 0;
+#if defined(_DEBUG)
+	createFactoryFlags = DXGI_CREATE_FACTORY_DEBUG;
+#endif
+
+	ThrowIfFailed(CreateDXGIFactory2(createFactoryFlags, IID_PPV_ARGS(&f4)));
+	DXGI_SWAP_CHAIN_DESC1 scDesc = {};
+	scDesc.Width = windowWidth;
+	scDesc.Height = windowHeight;
+	scDesc.BufferCount = numberOfBuffers;
+	scDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	scDesc.Stereo = FALSE;
+	scDesc.SampleDesc = { 1, 0 }; // this must be so in the flip discard model https://en.wikipedia.org/wiki/Bit_blit
+	scDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	scDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+	scDesc.Scaling = DXGI_SCALING_STRETCH;
+	scDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	scDesc.Flags = checkTearingSupport() ? DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING : 0;
+
+	ThrowIfFailed(f4->CreateSwapChainForHwnd(commandQueue.Get(), hWnd, &scDesc, nullptr, nullptr, &swapChain1));
+
+	// disable alt + enter to handle full screen manually
+	ThrowIfFailed(f4->MakeWindowAssociation(hWnd, DXGI_MWA_NO_ALT_ENTER));
+
+	ThrowIfFailed(swapChain1.As(&swapChain4));
+	return swapChain4;
 }
