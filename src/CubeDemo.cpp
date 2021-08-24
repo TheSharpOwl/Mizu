c609af536609927df6e987760d80878e308b7dec
TheSharpOwl <ziadxkabakibi@hotmail.com>
2021-08-24 10:34:31 +0300

    Added shaders loading + comment about what to do next time.

--- src/CubeDemo.cpp
+++ src/CubeDemo.cpp
@@ -91,6 +91,20 @@ bool CubeDemo::LoadContent()
 
 
 	// Loading shaders
+	cp<ID3DBlob> vertexShaderBlob;
+	ThrowIfFailed(D3DReadFileToBlob(L"VertexShader.cso", &vertexShaderBlob));
+
+	cp<ID3DBlob> pixelShaderBlob;
+	ThrowIfFailed(D3DReadFileToBlob(L"PixelShader.cso", &pixelShaderBlob));
+
+	// Create the vertex input layout
+	D3D12_INPUT_ELEMENT_DESC inputLayout[] = {
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+	};
+
+	// TODO FOR NEXT TIME create a root signature
+
 	return false;
 }
 
