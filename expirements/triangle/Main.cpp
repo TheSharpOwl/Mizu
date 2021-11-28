21691ba0eba97d6341041ffc137905af7e7324a2
TheSharpOwl <ziadxkabakibi@hotmail.com>
2021-11-28 05:32:00 +0300

    Removed the tutorial + added the microsoft triangle sample intitially.

---
+++ expirements/triangle/Main.cpp
@@ -0,0 +1,29 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+// 
+// Indicates to hybrid graphics systems to prefer the discrete part by default
+
+
+#include "stdafx.h"
+#include "D3D12HelloTriangle.h"
+
+extern "C"
+{
+    __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
+_Use_decl_annotations_
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+    D3D12HelloTriangle sample(1280, 720, L"D3D12 Hello Triangle");
+    return Win32Application::Run(&sample, hInstance, nCmdShow);
+}
