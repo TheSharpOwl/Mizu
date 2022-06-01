f0b64ebc11a732ff6e0488046d047b9da9cdf8d8
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2022-06-01 17:34:57 +0300

    Draw every node with a thread.

--- Experiments/ThousandTriangles/resources/MeshShader.hlsl
+++ Experiments/ThousandTriangles/resources/MeshShader.hlsl
@@ -16,27 +16,30 @@ ConstantBuffer<SubsetIndicator> subsetStart : register(b0);
 
 
 [outputtopology("triangle")]
-[numthreads(75, 1, 1)]
+[numthreads(96, 1, 1)]
 void main(
 	in uint3 groupID : SV_GroupID,
 	in uint3 threadInGroup : SV_GroupThreadID,
-	out vertices MSvert verts[225],
-	out indices uint3 idx[75]) // Three indices per primitive
+	out vertices MSvert verts[96],
+	out indices uint3 idx[32]) // Three indices per primitive
 {
-	const uint numVertices = 225;
-	const uint numPrimitives = 75; // 225 / 3
+	const uint numVertices = 96;
+	const uint numPrimitives = 32; // 96 / 3
 	SetMeshOutputCounts(numVertices, numPrimitives);
 
 	uint tid = threadInGroup.x;
+	uint numOfTriangles = 4800 / 1;
 
-	// each thread 1 triangle
-	for(int i = 0; i < 3; i++)
+	int k = subsetStart.start + tid;
+
+	if (k < numOfTriangles * 3)
 	{
-		int j = tid * 3 + i;
-		int k = subsetStart.start + j;
-		verts[j].pos = float4(coords[k].x, coords[k].y, coords[k].z, 1.f);
-		verts[j].color = float4(coords[k].w, 1.f, coords[k].w, 1.f);
+		verts[tid].pos = float4(coords[k].x, coords[k].y, coords[k].z, 1.f);
+		verts[tid].color = float4(coords[k].w, 1.f, coords[k].w, 1.f);
 	}
 
-	idx[tid] = uint3(tid * 3, tid * 3 + 1, tid * 3 + 2);
+	if(tid < 32 && subsetStart.start + tid * 3 < numOfTriangles * 3)
+	{
+		idx[tid] = uint3(tid * 3, tid * 3 + 1, tid * 3 + 2);
+	}
 }
