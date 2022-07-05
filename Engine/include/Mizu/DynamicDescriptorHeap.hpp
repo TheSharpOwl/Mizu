2612e306b5f095688e0775cfa29c0c99ea3c5605
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2022-07-05 23:58:53 +0300

    Add commit functions.

--- Engine/include/Mizu/DynamicDescriptorHeap.hpp
+++ Engine/include/Mizu/DynamicDescriptorHeap.hpp
@@ -12,9 +12,9 @@ namespace Mizu
 	class DynamicDescriptorHeap
 	{
 	public:
-		DynamicDescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE heapType, uint32_t descriptorsPerHeap = 1024) {}
+		DynamicDescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE heapType, uint32_t descriptorsPerHeap = 1024);
 
-		virtual ~DynamicDescriptorHeap() {};
+		virtual ~DynamicDescriptorHeap();
 
 		/**
 		 * \brief Stages a contiguous range of CPU visible descriptors while they are not copied to the GPU visible descriptor heap until commitStagedDescriptors function is called
@@ -24,6 +24,21 @@ namespace Mizu
 		 * \param srcDescriptors base descriptor to start copying descriptors from
 		 */
 		void stageDescriptors(uint32_t rootParameterIdx, uint32_t offset, uint32_t descriptorsCount, const D3D12_CPU_DESCRIPTOR_HANDLE srcDescriptors) {}
+
+
+		/**
+		 * \brief Copies all of the staged descriptors to GPU's visible descriptor heap and bindsthe desciptors heap and descriptor tables to the command list.
+		 * \param commandList 
+		 * \param setFunc Used to set the GPU visible descriptors on the command list, two possible possible functions are :
+		 * 1. SetGraphicsRootDescriptorTable before a draw
+		 * 2. SetComputeRootDescriptorTable	 before a dispatch
+		 *
+		 * or it might not know which function to use so it will be passed as an arg // TODO improve this documentation
+		 */
+		void commitStagedDescriptors(CommandList& commandList, std::function<void(ID3D12GraphicsCommandList*, UINT, D3D12_GPU_DESCRIPTOR_HANDLE)> setFunc);
+		void commitStagedDescriptorsForDraw(CommandList& commandList);
+		void commitStagedDescriptorsForDispatch(CommandList& commandList);
+
 
 	};
 }
--- Engine/src/DynamicDescriptorHeap.cpp
+++ Engine/src/DynamicDescriptorHeap.cpp
@@ -1,5 +1,26 @@
+#include "DynamicDescriptorHeap.hpp"
 
 namespace Mizu
 {
-	
+	DynamicDescriptorHeap::DynamicDescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE heapType, uint32_t descriptorsPerHeap)
+	{
+		
+	}
+	DynamicDescriptorHeap::~DynamicDescriptorHeap()
+	{
+
+	};
+
+	void DynamicDescriptorHeap::commitStagedDescriptors(CommandList& commandList, std::function<void(ID3D12GraphicsCommandList*, UINT, D3D12_GPU_DESCRIPTOR_HANDLE)> setFunc)
+	{
+
+	}
+	void DynamicDescriptorHeap::commitStagedDescriptorsForDraw(CommandList& commandList)
+	{
+
+	}
+	void DynamicDescriptorHeap::commitStagedDescriptorsForDispatch(CommandList& commandList)
+	{
+
+	}
 }
