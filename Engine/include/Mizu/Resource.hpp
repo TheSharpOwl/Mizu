3bfc475db78af57826db9d7cb00b5a040791c9d0
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2022-08-01 11:06:42 +0300

    Add initial resource class.

--- Engine/include/Mizu/Resource.hpp
+++ Engine/include/Mizu/Resource.hpp
@@ -1,9 +1,34 @@
 #pragma once
+#include "d3d12.h"
+#include "wrl.h"
+
+#include <string>
 
 namespace Mizu
 {
     class Resource
     {
+        Resource(const std::wstring& name = L"");
+        Resource(const D3D12_RESOURCE_DESC& resourceDesc, const D3D12_CLEAR_VALUE* clearValue = nullptr, const std::wstring& name = L"");
+        Resource(Microsoft::WRL::ComPtr<ID3D12Resource> resource, const std::wstring& name = L"");
+        Resource(const Resource& copy);
+        Resource(Resource&& copy);
+        Resource& operator=(const Resource& other);
+        Resource& operator=(Resource&& copy);
+
+
+
+        virtual ~Resource();
+
+
+        bool isValid() const
+        {
+            return m_resource != nullptr;
+        }
+
+    private:
+
+        Microsoft::WRL::ComPtr<ID3D12Resource> m_resource;
 
     };
 }
--- Engine/src/Resource.cpp
+++ Engine/src/Resource.cpp
@@ -1,1 +1,16 @@
+#include <DX12LibPCH.h>
 #include "Mizu/Resource.hpp"
+
+
+namespace Mizu
+{
+    Resource::Resource(const std::wstring& name) {}
+    Resource::Resource(const D3D12_RESOURCE_DESC& resourceDesc, const D3D12_CLEAR_VALUE* clearValue, const std::wstring& name) {}
+    Resource::Resource(Microsoft::WRL::ComPtr<ID3D12Resource> resource, const std::wstring& name) {}
+    Resource::Resource(const Resource& copy) {}
+    Resource::Resource(Resource&& copy) {}
+    Resource& Resource::operator=(const Resource& other) { return *this; }
+    Resource& Resource::operator=(Resource&& copy) { return *this; }
+
+	Resource::~Resource(){}
+}
