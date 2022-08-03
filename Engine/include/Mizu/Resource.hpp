025d995d584c6724337f8cd1ad759bd8157e3382
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2022-08-03 23:34:30 +0300

    Finish adding resource class.

--- Engine/include/Mizu/Resource.hpp
+++ Engine/include/Mizu/Resource.hpp
@@ -6,9 +6,12 @@
 
 namespace Mizu
 {
-    class Resource
+	/**
+	 * \brief wrapper for direct3d 12 resource and provides a base class for other types of resources such as textures and buffers
+	 */
+	class Resource
     {
-        Resource(const std::wstring& name = L"");
+        Resource(std::wstring name = L"");
 
         Resource(const D3D12_RESOURCE_DESC& resourceDesc, const D3D12_CLEAR_VALUE* clearValue = nullptr, const std::wstring& name = L"");
 
@@ -20,7 +23,7 @@ namespace Mizu
 
 
         Resource& operator=(const Resource& other);
-        Resource& operator=(Resource&& copy);
+        Resource& operator=(Resource&& other);
 
 
         virtual ~Resource();
@@ -48,14 +51,27 @@ namespace Mizu
             return result;
         }
 
+        // should only be called by the command list and the name will stay the same unless setName(std::wstring) is called
         virtual void setResource(Microsoft::WRL::ComPtr<ID3D12Resource> resource, const D3D12_CLEAR_VALUE* clearValue = nullptr);
 
+        /**
+         * \brief returns the SRV for a resource
+         * \param srvDesc description of the SRV to return. (default is nullptr which returns the default SRV for the resource (the SRV that is created with no description provided)
+         * \return 
+         */
         virtual D3D12_CPU_DESCRIPTOR_HANDLE getShaderResourceView(const D3D12_SHADER_RESOURCE_VIEW_DESC* srvDesc = nullptr) const = 0;
 
+        /**
+         * \brief get UAV for a (sub)resource
+         * \param uavDesc  description of the UAV to return
+         * \return 
+         */
         virtual D3D12_CPU_DESCRIPTOR_HANDLE getUnorderedAccessView(const D3D12_UNORDERED_ACCESS_VIEW_DESC* uavDesc = nullptr) const = 0;
 
         void setName(const std::wstring& name);
 
+
+        // release the underlying resource which is useful for swap chain resizing
         virtual void resetResource();
 
     private:
--- Engine/src/Resource.cpp
+++ Engine/src/Resource.cpp
@@ -1,32 +1,124 @@
 #include <DX12LibPCH.h>
 #include "Mizu/Resource.hpp"
 
+#include "Mizu/Application.hpp"
+#include "Mizu/ResourceStateTracker.hpp"
 
 namespace Mizu
 {
-    Resource::Resource(const std::wstring& name) {}
-    Resource::Resource(const D3D12_RESOURCE_DESC& resourceDesc, const D3D12_CLEAR_VALUE* clearValue, const std::wstring& name) {}
-    Resource::Resource(Microsoft::WRL::ComPtr<ID3D12Resource> resource, const std::wstring& name) {}
-    Resource::Resource(const Resource& copy) {}
-    Resource::Resource(Resource&& copy) {}
-    Resource& Resource::operator=(const Resource& other) { return *this; }
-    Resource& Resource::operator=(Resource&& copy) { return *this; }
-
-	Resource::~Resource(){}
-
-
-    void setResource(Microsoft::WRL::ComPtr<ID3D12Resource> resource, const D3D12_CLEAR_VALUE* clearValue)
+    Resource::Resource(std::wstring name)
+	    : m_resourceName(std::move(name))
     {
 	    
     }
 
-    void setName(const std::wstring& name)
+	Resource::Resource(const D3D12_RESOURCE_DESC& resourceDesc, const D3D12_CLEAR_VALUE* clearValue, const std::wstring& name)
+    {
+        auto device = Application::Get().GetDevice();
+
+        if(clearValue)
+        {
+            m_clearValue = std::make_unique<D3D12_CLEAR_VALUE>(*clearValue);
+        }
+
+        auto heapProps = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
+		ThrowIfFailed(device->CreateCommittedResource(
+            &heapProps,
+            D3D12_HEAP_FLAG_NONE,
+            &resourceDesc,
+            D3D12_RESOURCE_STATE_COMMON,
+            m_clearValue.get(),
+            IID_PPV_ARGS(&m_resource)
+        ));
+
+        ResourceStateTracker::addGlobalResourceState(m_resource.Get(), D3D12_RESOURCE_STATE_COMMON);
+
+        setName(name);
+    }
+
+    Resource::Resource(Microsoft::WRL::ComPtr<ID3D12Resource> resource, const std::wstring& name)
+	    : m_resource(resource)
+    {
+        setName(name);
+    }
+
+    Resource::Resource(const Resource& copy)
+        : m_resource(copy.m_resource)
+        , m_resourceName(copy.m_resourceName)
+        , m_clearValue(std::make_unique<D3D12_CLEAR_VALUE>(*copy.m_clearValue))
+    {
+        int i = 3;
+    }
+
+    Resource::Resource(Resource&& copy)
+        : m_resource(std::move(copy.m_resource))
+        , m_resourceName(std::move(copy.m_resourceName))
+        , m_clearValue(std::move(copy.m_clearValue))
+    {
+    }
+
+    Resource& Resource::operator=(const Resource& other)
+    {
+        if(this != &other)
+        {
+            m_resource = other.m_resource;
+            m_resourceName = other.m_resourceName;
+            if(other.m_clearValue)
+            {
+                m_clearValue = std::make_unique<D3D12_CLEAR_VALUE>(*other.m_clearValue);
+            }
+        }
+
+	    return *this;
+    }
+    Resource& Resource::operator=(Resource&& other)
+    {
+        if(this != &other)
+        {
+            m_resource = other.m_resource;
+            m_resourceName = std::move(m_resourceName);
+            m_clearValue = std::move(other.m_clearValue);
+
+            other.m_resource.Reset();
+        }
+
+	    return *this;
+    }
+
+    // TODO try = default
+	Resource::~Resource()
     {
 	    
     }
 
-    void resetResource()
+
+    void Resource::setResource(Microsoft::WRL::ComPtr<ID3D12Resource> resource, const D3D12_CLEAR_VALUE* clearValue)
     {
-	    
+        m_resource = resource;
+        if(clearValue)
+        {
+            m_clearValue = std::make_unique<D3D12_CLEAR_VALUE>(*clearValue);
+        }
+        else
+        {
+            m_clearValue.reset();
+        }
+
+        setName(m_resourceName);
+    }
+
+    void Resource::setName(const std::wstring& name)
+    {
+        m_resourceName = name;
+        if(m_resource && !m_resourceName.empty())
+        {
+            m_resource->SetName(m_resourceName.c_str());
+        }
+    }
+
+    void Resource::resetResource()
+    {
+        m_resource.Reset();
+        m_clearValue.reset();
     }
 }
