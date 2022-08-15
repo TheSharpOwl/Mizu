19c0d7202b40545baa3b29b6ee631ac6e774a232
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2022-08-15 23:36:45 +0300

    Add TransitionBarrier function to CommandList class.

--- Engine/include/Mizu/CommandList.hpp
+++ Engine/include/Mizu/CommandList.hpp
@@ -3,22 +3,34 @@
 
 namespace Mizu
 {
+    class Resource;
+    class ResourceStateTracker;
+
     class CommandList
     {
     public:
-        Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> getGraphicsCommandList()
+        Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> getGraphicsCommandList() // TODO
         {
             return m_commandList;
         }
 
-        void setDescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE descriptorHeapType, ID3D12DescriptorHeap* descriptorHeap)
+        void setDescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE descriptorHeapType, ID3D12DescriptorHeap* descriptorHeap) // TODO
         {
 	        
+	        
+        }
+
+        void TransitionBarrier(const Resource& resource, D3D12_RESOURCE_STATES stateAfter, UINT subResource, bool flushBarriers);
+
+        void flushResourceBarriers() // TODO
+        {
 	        
         }
 
     private:
         Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_commandList;
+
+        std::unique_ptr<ResourceStateTracker> m_resourceStateTracker;
     };
 }
 
--- Engine/src/CommandList.cpp
+++ Engine/src/CommandList.cpp
@@ -1,1 +1,23 @@
 #include "CommandList.hpp"
+#include "Resource.hpp"
+#include "ResourceStateTracker.hpp"
+
+
+namespace Mizu
+{
+	void CommandList::TransitionBarrier(const Resource& resource, D3D12_RESOURCE_STATES stateAfter, UINT subResource, bool flushBarriers)
+	{
+		auto pResource = resource.getResource();
+		if(pResource)
+		{
+			// we don't care about the before state because the resource state tracker will fix it for us
+			auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(pResource.Get(), D3D12_RESOURCE_STATE_COMMON, stateAfter, subResource);
+			m_resourceStateTracker->resourceBarrier(barrier);
+		}
+
+		if(flushBarriers)
+		{
+			flushResourceBarriers();
+		}
+	}
+}
