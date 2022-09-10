c99deb06faefd6b395c3fbea5c85cecaea5e4904
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2022-09-10 18:14:44 +0300

    Update CommandList class.

--- Engine/include/Mizu/CommandList.hpp
+++ Engine/include/Mizu/CommandList.hpp
@@ -5,10 +5,19 @@ namespace Mizu
 {
     class Resource;
     class ResourceStateTracker;
+    class UploadBuffer;
 
     class CommandList
     {
     public:
+        CommandList(D3D12_COMMAND_LIST_TYPE type);
+        virtual ~CommandList();
+
+
+        //D3D12_COMMAND_LIST_TYPE getCommandListType() const; TODO
+
+
+
         Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> getGraphicsCommandList() // TODO
         {
             return m_commandList;
@@ -20,17 +29,29 @@ namespace Mizu
 	        
         }
 
-        void TransitionBarrier(const Resource& resource, D3D12_RESOURCE_STATES stateAfter, UINT subResource, bool flushBarriers);
+        void copyResource(Resource& dstRes, const Resource& srcRes);
+
+        void transitionBarrier(const Resource& resource, D3D12_RESOURCE_STATES stateAfter, UINT subResource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES, bool flushBarriers = false);
 
         void flushResourceBarriers() // TODO
         {
 	        
         }
 
+        // the root parameter at rootParameterIndex should be set to D3D12_ROOT_PARAMETER_TYPE_CBV
+        void setGraphicsDynamicConstantBuffer(uint32_t rootParameterIndex, size_t sizeInBytes, const void* bufferData);
+
     private:
+
+        void trackResource(const Resource& res);
+
+        void trackObject(Microsoft::WRL::ComPtr<ID3D12Object> object);
+
         Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_commandList;
 
         std::unique_ptr<ResourceStateTracker> m_resourceStateTracker;
+
+        std::unique_ptr<UploadBuffer> m_uploadBuffer;
     };
 }
 
--- Engine/src/CommandList.cpp
+++ Engine/src/CommandList.cpp
@@ -1,23 +1,58 @@
 #include "CommandList.hpp"
 #include "Resource.hpp"
 #include "ResourceStateTracker.hpp"
+#include "UploadBuffer.hpp"
 
 
 namespace Mizu
 {
-	void CommandList::TransitionBarrier(const Resource& resource, D3D12_RESOURCE_STATES stateAfter, UINT subResource, bool flushBarriers)
+	void CommandList::transitionBarrier(const Resource& resource, D3D12_RESOURCE_STATES stateAfter, UINT subResource, bool flushBarriers)
 	{
 		auto pResource = resource.getResource();
-		if(pResource)
+		if (pResource)
 		{
 			// we don't care about the before state because the resource state tracker will fix it for us
 			auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(pResource.Get(), D3D12_RESOURCE_STATE_COMMON, stateAfter, subResource);
 			m_resourceStateTracker->resourceBarrier(barrier);
 		}
 
-		if(flushBarriers)
+		if (flushBarriers)
 		{
 			flushResourceBarriers();
 		}
 	}
+
+	void CommandList::copyResource(Resource& dstRes, const Resource& srcRes)
+	{
+		transitionBarrier(dstRes, D3D12_RESOURCE_STATE_COPY_DEST);
+		transitionBarrier(srcRes, D3D12_RESOURCE_STATE_COPY_SOURCE);
+
+		// to commit the resources
+		flushResourceBarriers();
+
+		m_commandList->CopyResource(dstRes.getResource().Get(), srcRes.getResource().Get());
+
+		// to track resources with temp life time only inside the command list
+		trackResource(dstRes);
+		trackResource(srcRes);
+	}
+
+	void CommandList::setGraphicsDynamicConstantBuffer(uint32_t rootParameterIndex, size_t sizeInBytes, const void* bufferData)
+	{
+		// const buffers must 256-bit aligned
+		auto heapAllocation = m_uploadBuffer->allocate(sizeInBytes, D3D12_CONSTANT_BUFFER_DATA_PLACEMENT_ALIGNMENT);
+		memcpy(heapAllocation.cpu, bufferData, sizeInBytes);
+		m_commandList->SetGraphicsRootConstantBufferView(rootParameterIndex, heapAllocation.gpu);
+	}
+
+	void CommandList::trackResource(const Resource& res)
+	{
+		trackObject(res.getResource());
+	}
+
+	void CommandList::trackObject(Microsoft::WRL::ComPtr<ID3D12Object> object)
+	{
+		// TODO
+	}
+
 }
